c30f3379a3330e84de53b9360960f615e3bc8579
Heather <larsen.hv@gmail.com>
2022-11-05 12:15:30 -0700

    Create ObservableList plus getters and setters

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,3 +1,63 @@
+import javafx.collections.ObservableList;
+
 public class Product {
 
+    private ObservableList<Part> associatedParts;
+    private int id, stock, min, max;
+    private String name;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void addAssociatedPart(Part part)
+
+    public ObservableList<Part> getAllAssociatedParts() {
+        return associatedParts;
+    }
 }
