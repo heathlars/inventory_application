170304bbc9e0b7fbf9a22cd6ed9bd36fb7b49dee
Heather <larsen.hv@gmail.com>
2022-11-05 12:05:05 -0700

    Create methods allParts, allProducts with getters

--- src/main/java/Inventory.java
+++ src/main/java/Inventory.java
@@ -1,2 +1,16 @@
+import javafx.collections.ObservableList;
+
 public class Inventory {
+
+    private ObservableList<Part> allParts;
+
+    private ObservableList<Product> allProducts;
+
+    public ObservableList<Part> getAllParts() {
+        return allParts;
+    }
+
+    public ObservableList<Product> getAllProducts() {
+        return allProducts;
+    }
 }
